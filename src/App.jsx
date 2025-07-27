bf5ed287160d50be30115cf042ae185bf880ba96
aryan9910 <aryanj9910@gmail.com>
2025-07-27 12:02:56 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import "./App.css";
+import Card from "./components/Card";
+
+const App = () => {
+  return (
+    <div className="container">
+      <h1>Aryan Jain</h1>
+      <h2>Frontend Developer</h2>
+      <div className="cards">
+        <Card
+          title="About me"
+          frontText="A passionate react developer from India"
+          backText="I specialize in building fast, responsive UI's using React and Firebase. I focus on performance, reusuability and clean code"
+        />
+        <Card
+          title="Experience"
+          frontText="Cognizant-Alumni System"
+          backText="Built a full featured Alumni portal using React, Redux, and Github with smooth navigation and mobile responsiveness"
+        />
+        <Card
+          title="Projects"
+          frontText="Chat App, Vid-Tube, Text-Utils"
+          backText="Chat-App: Firebase auth + chat | Vid-Tube: Youtube API | Text-Utils: Text analysis tools"
+          link="https://github.com/aryan9910"
+        />
+        <Card title="Contact" frontText="Reach out to me" backText="Email: aryanj9910@gmail.com | Phone: 9600620485" link="mailto:aryanj9910@gmail.com" />
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Card.jsx
@@ -0,0 +1,18 @@
+import './Card.css'
+
+export default function Card({title, frontText, backText, link}){
+    return(
+        <div className="card">
+            <div className="card-inner">
+                <div className="card-front">
+                    <h2>{title}</h2>
+                    <p>{frontText}</p>
+                </div>
+                <div className="card-back">
+                    <p>{backText}</p>
+                    {link && <a href={link} target="_blank" rel='noopener noreferrer'>View more</a>}
+                </div>
+            </div>
+        </div>
+    )
+}
